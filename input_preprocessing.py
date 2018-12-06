d9bfdf774615c37bfc80ac533daead026f732c65
tskim <rlaxotmd9960@gmail.com>
2018-12-06 19:56:58 +0900

    stock data preprocess

--- input_preprocessing.py
+++ input_preprocessing.py
@@ -10,159 +10,106 @@ from math import log
 from mpl_toolkits.mplot3d import axes3d
 from sklearn.model_selection import cross_val_score
 from collections import OrderedDict
-import tensorflow as tf
+import input_prepro_function as func
 
 n_train_begin = 0
-n_train_end = 3111
+n_train_end = 3351
 
-n_test_begin = 3111
-n_test_end = 3853
+n_test_begin = 3351
+n_test_end = 4306
 
-kospi = np.array((pd.read_csv('/home/tskim/바탕화면/kospi(00.1.1_16.2.12).csv')).dropna()) #미국 주가 제외 x
-sp = np.array((pd.read_csv('/home/tskim/바탕화면/s&p(00.1.1_16.2.12).csv')).dropna())
+kospi = np.array((pd.read_csv('/home/tskim/바탕화면/kospi.csv')).dropna()) #미국 주가 제외 x
+sp = np.array((pd.read_csv('/home/tskim/바탕화면/s&p500.csv')).dropna())
 
-kospi_input_dic = OrderedDict() #input dictionary
-sp_input_dic = OrderedDict()
-for i in range(len(kospi)):
-    kospi_input_dic["".join(kospi[:, :1][i])] = kospi[:, 1:7][i]
+# make numpy(kospi,s&p) dictionary
+kospi_input_dic = func.getInDic(kospi)
+sp_input_dic = func.getInDic(sp)
 
-for i in range(len(sp)):
-    sp_input_dic["".join(sp[:,:1][i])] = sp[:,1:7][i]
+# get answer
+kospi_updown = func.getCloseChange(kospi_input_dic)
 
-sl = [] # s&p close price change
-for i in range(0,len(sp)-1): # s&p500 close price change
-    sl.append(sp[i+1,4:5] - sp[i,4:5])
+# for direct comparison with kospi
+sp_updown = func.getCloseChange(sp_input_dic)
 
-del sp_input_dic[sp[0,0]]# first value remove for append
-sp = np.delete(sp,0,axis=0)
+# make feature(당일 종가 - 전일 종가)
+sp_close_minus = func.getCloseMinus(sp_input_dic)
+kospi_close_minus = func.getCloseMinus(kospi_input_dic)
 
-i=0
-for k,v in sp_input_dic.items():#하루 전 종가등락 dic에 병합
-    sp_input_dic[k] = np.append(sp_input_dic[k],sl[i])
-    i=i+1
+# merge 6feature with 당일 종가-전일 종가
+sp_input_dic_closeMinus = func.mergeDics(sp_input_dic,sp_close_minus)
+kospi_input_dic_closeMinus = func.mergeDics(kospi_input_dic,kospi_close_minus)
 
-rl_past = [] # kospi close price change for target on One day before
+# zscore(feature)
+kospi_input_dic_closeMinus_z = func.makeZscore(kospi_input_dic_closeMinus)
+sp_input_dic_closeMinus_z = func.makeZscore(sp_input_dic_closeMinus)
 
-for i in range(0, len(kospi) - 1): # label
-    rl_past.append(kospi[i+1,4:5] - kospi[i, 4:5])
+#merge kospi feature with s&p feature
+final_input = func.mergeDics(kospi_input_dic_closeMinus_z,sp_input_dic_closeMinus_z)
+# -> 이때 final_input 안의 value들은 key값에 있는 날짜 다음날의 종가 등락에 영향을 주는 features
 
-del kospi_input_dic[kospi[0,0]]
-kospi = np.delete(kospi,0,axis=0)
+#answer와 input날짜에 맞게 병합 (answer를 final_input의 날짜에 맞추기 위한 병합)
+answerPlusFinalInput = func.mergeDics(final_input,kospi_updown)
+sp_answerPlusFinalInput = func.mergeDics(final_input,sp_updown)
 
-a=0
-for k,v in kospi_input_dic.items():#하루 전 종가등락 dic에 병합
-   kospi_input_dic[k] = np.append(kospi_input_dic[k],rl_past[a])
-   a=a+1
-##################### 한국 날짜 미국 날짜 같은 날만 남기고 제거
-input = OrderedDict()
-date = [] # 한국 미국 겹치는 날짜
-for k,v in kospi_input_dic.items():
-    arr = []
-    if k in sp_input_dic:
-        arr.append(kospi_input_dic[k])
-        arr.append(sp_input_dic[k])
-        input[k] = arr
-        date.append(k)
-#####################making target
-input_kospi = []
-input_sp = []
-for k,v in input.items():
-    input_kospi.append(v[0])
-    input_sp.append(v[1])
+kospi_updown_adjusted = OrderedDict() # final answer
+final_input_adjusted = OrderedDict() # final input
+for k,v in answerPlusFinalInput.items():
+    kospi_updown_adjusted[k] = v[14:]
+    final_input_adjusted[k] = v[:14]
 
-answer=[] #answer
-for i in range(len(input)-1):
-    if input_kospi[i+1][3] > input_kospi[i][3]:
-        answer.append(1) #종가 상승시 1
-    else:
-        answer.append(0) #종가 하락,유지시 0
-a = answer[:]
-s=[] #미국 종가 등락 baseline classification용
-for i in range(len(input_sp)-1):
-    if input_sp[i+1][3] > input_sp[i][3]:
-        s.append(1) #종가 상승시 1
-    else:
-        s.append(0) #종가 하락,유지시 0
-del a[0]
-del s[-1]
+sp_updown_adjusted = OrderedDict() # direct comparison을 위한 변수
+for k,v in sp_answerPlusFinalInput.items():
+    sp_updown_adjusted[k] = v[14:]
 
-print(" s&p, kospi direct comparison acc :  " + str(accuracy_score(a,s))) #0.60591
+sp_answer = [] # make sp_updown_adjusted integer
+for k,v in sp_updown_adjusted.items():
+    a=[]
+    a.append(int(v[0]))
+    a.append(int(v[1]))
 
-def make_z_score(input):
-    open = []
-    high = []
-    low = []
-    close = []
-    adjClose = []
-    volume = []
-    ud = []
-    for i in input:
-        open.append(i[0])
-        high.append(i[1])
-        low.append(i[2])
-        close.append(i[3])
-        adjClose.append(i[4])
-        volume.append(i[5])
-        ud.append(i[6])
-    open_z = stats.zscore(open)
-    high_z = stats.zscore(high)
-    low_z = stats.zscore(low)
-    close_z = stats.zscore(close)
-    adjClose_z = stats.zscore(adjClose)
-    volume_z = stats.zscore(volume)
-    ud_z = stats.zscore(ud)
-    a_a = []
-    for i in range(0,len(input)):
-        k_a = []
-        k_a.append(open_z[i])
-        k_a.append(high_z[i])
-        k_a.append(low_z[i])
-        k_a.append(close_z[i])
-        k_a.append(adjClose_z[i])
-        k_a.append(volume_z[i])
-        k_a.append(ud_z[i])
-        a_a.append(k_a)
-    return a_a
+    sp_answer.append(a)
 
-k_input_z = make_z_score(input_kospi)
-sp_input_z = make_z_score(input_sp)
+#for baseline target (total size 4306)
+sp_direct_com = func.makeOneNum(sp_answer)
 
+# for direct comparison (test size)
+sp_direct_com_final = []
+for i in range(n_test_begin,n_test_end):
+    sp_direct_com_final.append(sp_direct_com[i])
 
-input_z = OrderedDict()
-i = 0
-for k,v in input.items():
-    all = []
-    all.append(k_input_z[i])
-    all.append(sp_input_z[i])
-    input_z[k] = all
-    i=i+1
-#######################################################여기까지
 temp = []
-input_z.popitem() #answer와 맞추기위해 마지막 value삭제
-del date[-1]
+date = []
+for k,v in final_input_adjusted.items():
+    date.append(k)
+    temp.append(v)
 
-for k,v in input_z.items(): # 2개 array의 kospi,s&p를 학습하기위해 하나의 array로 합침
-    for i in v[1]:
-        v[0].append(i)
-    temp.append(v[0])
+answer = [] # make kospi_updown_adjusted integer
+for k,v in kospi_updown_adjusted.items():
+    a=[]
+    a.append(int(v[0]))
+    a.append(int(v[1]))
+
+    answer.append(a)
+
+#for direct comparison (total size 4306)kospi
+kospi_all_com = func.makeOneNum(answer)
+
 train_X = []
 train_Y = []
-answer_n = [] #0 = [0,1] 1 = [1,0]
-for i in answer:
-    if i == 0:
-        answer_n.append([0,1])
-    else:
-        answer_n.append([1,0])
 
 for i in range(n_train_begin,n_train_end):
     train_X.append(temp[i])
-    train_Y.append(answer_n[i])
+    train_Y.append(answer[i])
 
 test_X = []
 test_Y = []
-for i in range(n_test_begin+1,n_test_end):
+
+for i in range(n_test_begin,n_test_end):
     test_X.append(temp[i])
-    test_Y.append(answer_n[i])
-print("train_date : " + date[n_train_begin] +" ~ " + date[n_train_end])
-print("test_date : " + date[n_test_begin+1] +" ~ " + date[n_test_end-1])
-#####################
+    test_Y.append(answer[i])
+
+train_Y_oneNum = func.makeOneNum(train_Y)
+test_Y_oneNum = func.makeOneNum(test_Y)
+
+print("train_date : " + date[n_train_begin] +" ~ " + date[n_train_end-1])
+print("test_date : " + date[n_test_begin] +" ~ " + date[n_test_end-1])
